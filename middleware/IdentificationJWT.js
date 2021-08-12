a16117d5cc5854f264a7d9c2a6d9313c8d354834
worna <61627538+worna@users.noreply.github.com>
2021-08-12 15:33:41 +0200

    Update IdentificationJWT.js

--- middleware/IdentificationJWT.js
+++ middleware/IdentificationJWT.js
@@ -20,9 +20,11 @@ module.exports.identification = async (req, res, next) => {
     const headerAuth = req.get('authorization');
     if(headerAuth !== undefined && headerAuth.includes("Bearer")){
         const jwtToken =  headerAuth.split(' ')[1];
+        console.log(jwtToken);
         try{
             const decodedJwtToken = jwt.verify(jwtToken, process.env.SECRET_TOKEN);
             req.session = decodedJwtToken.value;
+            console.log(req.session.email);
             req.session.authLevel = decodedJwtToken.status;
             next();
         }
